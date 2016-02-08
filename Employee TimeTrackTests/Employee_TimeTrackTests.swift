fb0b3559f89e5403da00ba3267d0aa3a4464dc46
Asherlee Levy <asherleelevy@asherlees-macbook-pro.local>
2016-02-08 15:03:51 -0500

    Initial Commit

---
+++ Employee TimeTrackTests/Employee_TimeTrackTests.swift
@@ -0,0 +1,36 @@
+//
+//  Employee_TimeTrackTests.swift
+//  Employee TimeTrackTests
+//
+//  Created by Asherlee Levy on 2/8/16.
+//  Copyright © 2016 Asherlee Levy. All rights reserved.
+//
+
+import XCTest
+@testable import Employee_TimeTrack
+
+class Employee_TimeTrackTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
